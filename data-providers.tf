2ed2702567bf51a0826ef5dae086a0280b9698a5
olufolarin <folami25@gmail.com>
2022-08-05 17:15:33 +0100

    created multiple servers and assigned a subnet to each.

---
+++ data-providers.tf
@@ -0,0 +1,30 @@
+data "aws_subnets" "subnet_ids" {
+  filter {
+    name = "vpc-id"
+    values = [aws_default_vpc.default.id]
+  }
+}
+
+data "aws_ami" "regional_ami" {
+  most_recent = true
+  owners = ["amazon"]
+
+  filter {
+    name = "name"
+    values = ["amzn2-ami-kernel-5.10-hvm-*"]
+  }
+
+  filter {
+    name = "architecture"
+    values = ["x86_64"]
+  }
+}
+
+data "aws_ami_ids" "linux_ids" {
+  owners = [ "amazon" ]
+
+  filter {
+    name = "name"
+    values = [ "amazn2-ami-hvm-*" ]
+  }
+}
---
+++ resources.tf
@@ -0,0 +1,66 @@
+resource "aws_default_vpc" "default" {
+  
+}
+
+resource "aws_security_group" "security_group" {
+  name = "http_servers"
+  vpc_id = aws_default_vpc.default
+
+  ingress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  } 
+
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = [ "0.0.0.0/0" ]
+  }
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = -1
+    cidr_blocks = [ "0.0.0.0/0" ]
+  }
+
+  tags = {
+    name = "http_servers_security_group"
+  }
+}
+
+resource "aws_instance" "http_servers" {
+  ami = 
+  key_name = "ec2-project1"
+  instance_type = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.security_group.id]
+
+# create an ec2 instance for each subnet
+for_each = data.aws_subnets.subnet_ids.ids
+  subnet_id = each.value
+
+  tags = [
+    name: "http_servers_${each.value}"
+  ]
+
+  connection {
+    type = "ssh"
+    host = self.public_ip
+    user = "ec2-user"
+    private_key = file(var.aws_key_pair)
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      # install httpd/http server
+      "sudo yum install httpd -y",
+      # start the server
+      "sudo service httpd start",
+      # copy a file
+      "echo Here is a virtual server hosted on aws with an html file on it displaying this message for all to see. The virtual server is at ${self.public_dns}. | sudo tee /var/www/html/index.html"
+    ]
+  }
+}
